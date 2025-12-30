8eca750a630824b20d4166e20d41d0935f4cce65
Nikolai Trusov <ntrusov@umschool.ru>
2025-12-30 15:47:54 +0300

    fix: ruff

--- src/app/bootstrap/ioc/application.py
+++ src/app/bootstrap/ioc/application.py
@@ -1,12 +1,24 @@
 from dishka import Provider, Scope, WithParents, provide_all
 
-from app.application.interactors.admin.count_users import CountUsersAdminInteractor
-from app.application.interactors.admin.create_user_admin import CreateUserAdminInteractor
-from app.application.interactors.admin.delete_users_admin import DeleteUsersAdminInteractor
-from app.application.interactors.admin.get_user_by_id import GetUserByIdAdminInteractor
+from app.application.interactors.admin.count_users import (
+    CountUsersAdminInteractor,
+)
+from app.application.interactors.admin.create_user_admin import (
+    CreateUserAdminInteractor,
+)
+from app.application.interactors.admin.delete_users_admin import (
+    DeleteUsersAdminInteractor,
+)
+from app.application.interactors.admin.get_user_by_id import (
+    GetUserByIdAdminInteractor,
+)
 from app.application.interactors.admin.get_users import GetUsersAdminInteractor
-from app.application.interactors.admin.get_users_by_ids import GetUsersByIdsAdminInteractor
-from app.application.interactors.admin.update_user_admin import UpdateUserAdminInteractor
+from app.application.interactors.admin.get_users_by_ids import (
+    GetUsersByIdsAdminInteractor,
+)
+from app.application.interactors.admin.update_user_admin import (
+    UpdateUserAdminInteractor,
+)
 from app.application.interactors.user.create_user import CreateUserInteractor
 from app.application.interactors.user.get_user import GetUserInteractor
 from app.application.interactors.user.update_user import UpdateUserInteractor
