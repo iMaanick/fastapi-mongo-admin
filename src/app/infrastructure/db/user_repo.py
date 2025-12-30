bda49147a5cb85a86f65f03ebe31c0c1010c114a
Nikolai Trusov <ntrusov@umschool.ru>
2025-12-30 16:06:46 +0300

    fix: repo delete

--- src/app/infrastructure/db/user_repo.py
+++ src/app/infrastructure/db/user_repo.py
@@ -10,7 +10,6 @@ from motor.motor_asyncio import (
     AsyncIOMotorDatabase,
 )
 
-from app.application.exceptions.base import EntityNotFoundError
 from app.application.user_repo import UserRepository
 from app.domain.model import User
 
@@ -97,10 +96,6 @@ class MongoUserRepository(UserRepository):
 
         if result.deleted_count == 0:
             logger.warning("User not found for deletion: %s", user_id)
-            raise EntityNotFoundError(
-                entity_type=User,
-                field_name="_id",
-                field_value=user_id,
-            )
+            return
 
         logger.info("User deleted: %s", user_id)
